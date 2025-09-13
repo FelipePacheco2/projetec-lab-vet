31bf3e371685a08c6d98db926f085ce838e0e931
Felipe Pacheco <felipecaballeropacheco@gmail.com>
2025-09-13 16:10:35 -0400

    ProjetoVetLab

--- ProjetoVetLab/src/App.java
+++ ProjetoVetLab/src/App.java
@@ -1,22 +1,18 @@
 import java.util.ArrayList;
 
 import Dominio.Cliente;
+import FakeBD.ClienteFakeDB;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        Cliente c1 = new Cliente();
 
-        c1.setCodigo(1);
-        c1.setNome("Felipe");
-        c1.setEmail("felipe@gmail.com");
-        c1.setCpf("98765432112");
-        c1.setNumeroCartao("123");
-
-        ArrayList<Cliente> clientes = new ArrayList<>();
-        clientes.add(c1);
-        
-        Cliente c2 = new Cliente(2, "Rayssa", "123654789", "rayssa@gmail.com", "123");
-        clientes.add(c2);
+        ClienteFakeDB db = new ClienteFakeDB();
+        ArrayList<Cliente> clientes = db.getInstancia();
+        for (Cliente cliente : clientes){
+            System.out.println("Codigo: " + cliente.getCodigo());
+            System.out.println("nome: " + cliente.getNome());
+            System.out.println("CPF: " + cliente.getCpf());
+        }
+    
     }
 }
